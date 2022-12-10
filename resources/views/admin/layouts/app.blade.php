aa01d182357c30a183cbf67d074a12e02c6205d5
Iman <imanali.cse@gmail.com>
2022-12-10 20:58:29 +0600

    login registration

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ config('app.name') }}</title>
+    </head>
+    <body>
+       <?php
+            $action = app('request')->route()->getAction();
+            $controller = class_basename($action['controller']);
+            list($controller, $action) = explode('@', $controller);
+        ?>
+
+{{--        @include('admin/includes/sidebar')--}}
+
+        <div class="content-wrapper">
+
+{{--            @include('admin/includes/navbar')--}}
+
+            <div class="main-container">
+{{--                @include('admin.includes.messages')--}}
+                @yield('content')
+            </div>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Admin\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,3 +24,8 @@ Route::post('/login', [LoginController::class, 'authenticate'])->name('login.sub
 
 Route::get('/registration', [RegisterController::class,'registration']);
 Route::post('/registration', [RegisterController::class, 'registrationSubmit'])->name('registration.submit');
+
+Route::prefix('admin')->namespace('Admin')->group(static function() {
+    Route::get('/', [DashboardController::class, 'index']);
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
